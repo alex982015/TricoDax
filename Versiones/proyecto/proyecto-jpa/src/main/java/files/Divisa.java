3d62ede525735d25653eb65954a60223b63cbb1e
Alex <alex982015marbella@gmail.com>
2022-03-23 19:19:00 +0100

    Inicio P1

---
+++ Versiones/proyecto/proyecto-jpa/src/main/java/files/Divisa.java
@@ -0,0 +1,80 @@
+package files;
+
+import java.util.Objects;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Divisa
+ *
+ */
+@Entity
+
+public class Divisa {
+
+	
+	@Id
+	private String abreviatura;
+	private String nombre;
+	private String simbolo;
+	private Double cambioEuro;
+	
+	public Divisa() {
+		super();
+	}
+
+	public String getAbreviatura() {
+		return abreviatura;
+	}
+
+	public void setAbreviatura(String abreviatura) {
+		this.abreviatura = abreviatura;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSimbolo() {
+		return simbolo;
+	}
+
+	public void setSimbolo(String simbolo) {
+		this.simbolo = simbolo;
+	}
+
+	public Double getCambioEuro() {
+		return cambioEuro;
+	}
+
+	public void setCambioEuro(Double cambioEuro) {
+		this.cambioEuro = cambioEuro;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(abreviatura);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Divisa other = (Divisa) obj;
+		return Objects.equals(abreviatura, other.abreviatura);
+	}
+
+	@Override
+	public String toString() {
+		return "Divisa [abreviatura=" + abreviatura + ", nombre=" + nombre + ", simbolo=" + simbolo + ", cambioEuro=" + cambioEuro + "]";
+	}
+   
+}
---
+++ Versiones/proyecto/proyecto-jpa/src/main/java/files/Main.java
@@ -0,0 +1,14 @@
+package files;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("proyecto");
+		EntityManager em = emf.createEntityManager();
+		em.close();
+		emf.close();
+	}
+}
